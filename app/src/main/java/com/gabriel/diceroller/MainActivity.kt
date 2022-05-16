e48cb35a8b06bbb63338cdc5da48f07fdb420580
Gabriel Silva <gabriel.psilva226@gmail.com>
2022-05-16 11:43:58 -0300

    Android Kotlin Fundamentals 01.3: Step 6 - Understand API levels and compatibility

--- app/src/main/java/com/gabriel/diceroller/MainActivity.kt
+++ app/src/main/java/com/gabriel/diceroller/MainActivity.kt
@@ -3,13 +3,19 @@ package com.gabriel.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        diceImage = findViewById(R.id.dice_image)
 
         val rollButton: Button = findViewById(R.id.roll_button)
         val countUpButton: Button = findViewById(R.id.count_up)
@@ -24,22 +30,22 @@ class MainActivity : AppCompatActivity() {
         val randomInt = (1..6).random()
 
 //        Toast.makeText(this, "Button clicked!", Toast.LENGTH_SHORT).show()
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
+
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
     }
 
     private fun countUp() {
-        val resultText: TextView = findViewById(R.id.result_text)
-
-        if (resultText.text == "Hello World!") {
-            resultText.text = "1"
-        } else if (resultText.text.toString().toInt() != 6) {
-            (resultText.text.toString().toInt() + 1).toString().also { resultText.text = it }
-        }
     }
 
     private fun reset() {
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = "0"
     }
 }
